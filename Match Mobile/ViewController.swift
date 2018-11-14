617e7eb27d2d1e9e8431c973efecfd5bdec16640
Pranav-Wadhwa <pranavwadhwa@outlook.com>
2018-11-14 14:35:16 -0500

    Meeting 1 Update

--- Match Mobile/ViewController.swift
+++ Match Mobile/ViewController.swift
@@ -10,6 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    
+    var selectedButton: UIButton?
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        if selectedButton == nil {
+            sender.alpha = 0.65
+            sender.isUserInteractionEnabled = false
+            selectedButton = sender
+        } else {
+            if sender.image(for: []) == selectedButton!.image(for: []) {
+                sender.alpha = 0
+                sender.isUserInteractionEnabled = false
+                selectedButton!.alpha = 0
+                selectedButton!.isUserInteractionEnabled = false
+            } else {
+                sender.alpha = 1
+                sender.isUserInteractionEnabled = true
+                selectedButton!.alpha = 1
+                selectedButton!.isUserInteractionEnabled = true
+            }
+            selectedButton = nil
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
